1532c9b72e64f82868b857844924a335df6e8778
FlechitUp <ximena.pocco@ucsp.edu.pe>
2019-06-19 11:11:27 -0400

    uploadi_g details

---
+++ src/test/java/com/bupho/rest/UserRestTest.java
@@ -0,0 +1,33 @@
+package com.bupho.rest;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.xime.bupho.BuphoApplication;
+import com.xime.bupho.model.UserModel;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = BuphoApplication.class)
+@WebAppConfiguration
+public class UserRestTest {
+
+ 
+
+}
